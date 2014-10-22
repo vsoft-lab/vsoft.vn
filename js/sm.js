756203a0a2505166d9d59828a5487ddabf7d8d5c
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-22 18:03:30 +0700

    submit action

---
+++ js/sm.js
@@ -0,0 +1,35 @@
+
+function isValidEmail(emailText) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailText);
+};
+
+$("#btnSub").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var comments = $("#comments").val();
+    
+    if(name === "" || email === "" || comments === "") {
+        swal("Oops...", "Please fill out all input box!", "error");
+        return false;
+    };
+    
+    if(!isValidEmail(email)) {
+        swal("Oops...", "Wrong email format!", "error");
+        return false;
+    }
+    
+    $.ajax({data: {
+        name: name,
+        email: email,
+        comments: comments
+    },
+            type: "POST",
+            url: "http://vsoft:1212/info",
+            success: function(res) {
+                $("#name").val("");
+                $("#email").val("");
+                $("#comments").val("");
+                swal("Good job!", "You clicked the button!", "success");
+            }});
+});
